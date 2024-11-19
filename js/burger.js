d737c50aa5c8d67b06801989f9b61abae60e289c
MaksKrut007 <mpushkov5@gmail.com>
2024-11-19 14:38:37 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,27 @@
+const burgerBtn = document.querySelector("#burger-btn");
+const burgerMenu = document.querySelector(".header-nav");
+
+burgerBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+    if (burgerMenu.classList.contains("open")) {
+      burgerclose();
+    }
+    else { 
+      burgerOpen();
+
+      document.addEventListener("click", burgerClose);
+    }
+  });
+
+  function burgerOpen() {
+    burgerMenu.classList.add("open");
+    burgerBtn.classList.add("active")
+
+    burgerMenu.addEventListener("click", (e) => e.stopPropagation());
+  }
+  function burgerClose() {
+    burgerMenu.classList.remove("open");
+    burgerBtn.classList.remove("active");
+
+    document.removeEventListener("click",burgerClose);
+  }
